290a692c7d5f7df2a661591cb0f7edec1a2fa420
emrekayik <emrekayik1905@gmail.com>
2024-12-27 03:19:25 +0300

    ui geliştirmeleri yapıldı

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const navbar = document.getElementById("navbar");
 
     window.addEventListener("scroll", () => {
-        setInterval(()=>{
         if (window.scrollY > 50) {
             navbar.classList.add("bg-primary", "text-white", "shadow-md", "backdrop-blur-md");
         } else {
             navbar.classList.remove("bg-primary", "text-white", "shadow-md", "backdrop-blur-md");
         }
-    },3000)
     });
 });
